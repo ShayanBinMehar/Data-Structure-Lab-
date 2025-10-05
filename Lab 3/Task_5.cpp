2f76dec100097a9ea5644d0bf6f77103b338ffc2
Muhammad Shayan Bin Mehar <shayansiddiqui300@gmail.com>
2025-10-05 20:35:30 +0500

    Add files via upload

--- Lab 3/Task_5.cpp
+++ Lab 3/Task_5.cpp
@@ -1,114 +1,199 @@
 #include <iostream>
 using namespace std;
 
-struct Node
+struct SinglyNode
 {
     int data;
-    Node *prev;
-    Node *next;
-    Node(int val) : data(val), prev(nullptr), next(nullptr) {}
+    SinglyNode *next;
+    SinglyNode(int val) : data(val), next(nullptr) {}
 };
 
-class Deque
+struct DoublyNode
 {
-    Node *front;
-    Node *rear;
+    int data;
+    DoublyNode *next;
+    DoublyNode *prev;
+    DoublyNode(int val) : data(val), next(nullptr), prev(nullptr) {}
+};
 
+class LinkedListConverter
+{
 public:
-    Deque() : front(nullptr), rear(nullptr) {}
-    void insertFront(int x)
+    SinglyNode *createSinglyList(int arr[], int n)
     {
-        Node *node = new Node(x);
-        if (!front)
+        if (n == 0)
+            return nullptr;
+
+        SinglyNode *head = new SinglyNode(arr[0]);
+        SinglyNode *current = head;
+
+        for (int i = 1; i < n; i++)
         {
-            front = rear = node;
+            current->next = new SinglyNode(arr[i]);
+            current = current->next;
         }
-        else
-        {
-            node->next = front;
-            front->prev = node;
-            front = node;
-        }
+
+        return head;
     }
-    void insertRear(int x)
+
+    DoublyNode *singlyToDoubly(SinglyNode *head)
     {
-        Node *node = new Node(x);
-        if (!rear)
+        if (head == nullptr)
+            return nullptr;
+
+        DoublyNode *dHead = new DoublyNode(head->data);
+        DoublyNode *dCurrent = dHead;
+        SinglyNode *sCurrent = head->next;
+        DoublyNode *dPrev = nullptr;
+
+        dCurrent->prev = dPrev;
+
+        while (sCurrent != nullptr)
         {
-            front = rear = node;
+            dPrev = dCurrent;
+            dCurrent->next = new DoublyNode(sCurrent->data);
+            dCurrent = dCurrent->next;
+            dCurrent->prev = dPrev;
+
+            sCurrent = sCurrent->next;
         }
-        else
-        {
-            rear->next = node;
-            node->prev = rear;
-            rear = node;
-        }
+
+        return dHead;
     }
-    void deleteFront()
+
+    SinglyNode *singlyToCircular(SinglyNode *head)
     {
-        if (!front)
+        if (head == nullptr)
+            return nullptr;
+
+        SinglyNode *current = head;
+
+        while (current->next != nullptr)
         {
-            cout << "Deque is empty!\n";
-            return;
+            current = current->next;
         }
-        Node *temp = front;
-        front = front->next;
-        if (front)
-            front->prev = nullptr;
-        else
-            rear = nullptr;
-        delete temp;
+
+        current->next = head;
+
+        return head;
     }
-    void deleteRear()
+
+    void printSinglyList(SinglyNode *head)
     {
-        if (!rear)
+        SinglyNode *current = head;
+        while (current != nullptr)
         {
-            cout << "Deque is empty!\n";
-            return;
-        }
-        Node *temp = rear;
-        rear = rear->prev;
-        if (rear)
-            rear->next = nullptr;
-        else
-            front = nullptr;
-        delete temp;
-    }
-    int getFront()
-    {
-        if (!front)
-        {
-            cout << "Deque is empty!\n";
-            return -1;
-        }
-        return front->data;
-    }
-    int getRear()
-    {
-        if (!rear)
-        {
-            cout << "Deque is empty!\n";
-            return -1;
-        }
-        return rear->data;
-    }
-    void printDeque()
-    {
-        Node *curr = front;
-        cout << "Deque: ";
-        while (curr)
-        {
-            cout << curr->data << " ";
-            curr = curr->next;
+            cout << current->data;
+            if (current->next != nullptr)
+            {
+                cout << " -> ";
+            }
+            current = current->next;
         }
         cout << endl;
     }
-    ~Deque()
+
+    void printDoublyList(DoublyNode *head)
     {
-        while (front)
+        DoublyNode *current = head;
+        while (current != nullptr)
         {
-            Node *temp = front;
-            front = front->next;
+            cout << current->data;
+            if (current->next != nullptr)
+            {
+                cout << " <-> ";
+            }
+            current = current->next;
+        }
+        cout << endl;
+    }
+
+    void printDoublyListReverse(DoublyNode *head)
+    {
+        if (head == nullptr)
+            return;
+
+        DoublyNode *current = head;
+
+        while (current->next != nullptr)
+        {
+            current = current->next;
+        }
+
+        while (current != nullptr)
+        {
+            cout << current->data;
+            if (current->prev != nullptr)
+            {
+                cout << " <-> ";
+            }
+            current = current->prev;
+        }
+        cout << endl;
+    }
+
+    void printCircularList(SinglyNode *head, int n)
+    {
+        if (head == nullptr)
+            return;
+
+        SinglyNode *current = head;
+        cout << "Circular list (first " << n << " nodes): ";
+
+        for (int i = 0; i < n; i++)
+        {
+            cout << current->data;
+            if (i < n - 1)
+            {
+                cout << " -> ";
+            }
+            current = current->next;
+        }
+        cout << " -> ... (loops back to " << head->data << ")" << endl;
+    }
+
+    void verifyCircular(SinglyNode *head)
+    {
+        if (head == nullptr)
+        {
+            cout << "Empty list" << endl;
+            return;
+        }
+
+        SinglyNode *slow = head;
+        SinglyNode *fast = head;
+
+        while (fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast)
+            {
+                cout << "✓ Verified: List is circular" << endl;
+                return;
+            }
+        }
+
+        cout << "✗ List is not circular" << endl;
+    }
+
+    void deleteSinglyList(SinglyNode *head)
+    {
+        while (head != nullptr)
+        {
+            SinglyNode *temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void deleteDoublyList(DoublyNode *head)
+    {
+        while (head != nullptr)
+        {
+            DoublyNode *temp = head;
+            head = head->next;
             delete temp;
         }
     }
@@ -116,22 +201,47 @@ public:
 
 int main()
 {
-    Deque dq;
-    dq.insertRear(10);
-    dq.insertRear(20);
-    dq.insertFront(5);
-    dq.insertFront(1);
-    dq.printDeque();                            // 1 5 10 20
-    cout << "Front: " << dq.getFront() << endl; // 1
-    cout << "Rear: " << dq.getRear() << endl;   // 20
-    dq.deleteFront();
-    dq.printDeque(); // 5 10 20
-    dq.deleteRear();
-    dq.printDeque(); // 5 10
-    dq.deleteFront();
-    dq.deleteRear();
-    dq.printDeque();  // empty
-    dq.deleteFront(); // error
-    dq.deleteRear();  // error
+    LinkedListConverter converter;
+
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "=== LINKED LIST CONVERSIONS ===" << endl;
+
+    SinglyNode *singlyList = converter.createSinglyList(arr, n);
+    cout << "\n1. Original Singly Linked List:" << endl;
+    converter.printSinglyList(singlyList);
+
+    cout << "\n2. Converted to Doubly Linked List:" << endl;
+    DoublyNode *doublyList = converter.singlyToDoubly(singlyList);
+    cout << "Forward:  ";
+    converter.printDoublyList(doublyList);
+    cout << "Backward: ";
+    converter.printDoublyListReverse(doublyList);
+
+    cout << "\n3. Converted to Circular Linked List:" << endl;
+    SinglyNode *circularList = converter.singlyToCircular(singlyList);
+    converter.printCircularList(circularList, 8);
+    converter.verifyCircular(circularList);
+
+    cout << "\n=== ANOTHER EXAMPLE ===" << endl;
+    int arr2[] = {10, 20, 30};
+    int n2 = sizeof(arr2) / sizeof(arr2[0]);
+
+    SinglyNode *singlyList2 = converter.createSinglyList(arr2, n2);
+    cout << "Original: ";
+    converter.printSinglyList(singlyList2);
+
+    DoublyNode *doublyList2 = converter.singlyToDoubly(singlyList2);
+    cout << "Doubly:   ";
+    converter.printDoublyList(doublyList2);
+
+    SinglyNode *circularList2 = converter.singlyToCircular(singlyList2);
+    converter.printCircularList(circularList2, 6);
+    converter.verifyCircular(circularList2);
+
+    converter.deleteDoublyList(doublyList);
+    converter.deleteDoublyList(doublyList2);
+
     return 0;
 }
